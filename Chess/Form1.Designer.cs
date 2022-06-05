724f0e627a050b82fbb4077ca2bfec2e79457595
jasper <jasper.desnyder@student.howest.be>
2022-06-05 10:29:54 +0200

    Tiles are added dynamically now (All with an image, need to fix that)

--- Chess/Form1.Designer.cs
+++ Chess/Form1.Designer.cs
@@ -34,9 +34,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Chess";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Chess/Form1.cs
+++ Chess/Form1.cs
@@ -16,5 +16,38 @@ namespace Chess
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    Button button = new Button();
+                    button.Location = new Point(j * 80, i * 80);
+                    button.Size = new Size(80, 80);
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 0;
+                    button.Tag = "Tile";
+
+                    if ((i + j) % 2 == 0)
+                        button.BackColor = Color.White;
+                    else button.BackColor = Color.Black;
+
+                    button.BackgroundImage = Image.FromFile(@"../images/rook-white.png");
+                    button.BackgroundImageLayout = ImageLayout.Stretch;
+                    button.BackgroundImage.Tag = "White rook";
+
+                    button.Click += Tile_Click;
+
+                    this.Controls.Add(button);
+                }
+            }
+        }
+
+        private void Tile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Click!");
+        }
     }
 }
